700bcbf77bbf4290361bfcb79a4765c739fe18ca
Almaz Zakariev <almazori228@gmail.com>
2024-04-14 17:38:48 +0300

    toggle for education

---
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject showEducationToggleGO;
+    public bool test;
+    public static bool ShowEducation = true;
+
+
+    private void Awake()
+    {
+        var showEducationTugle= showEducationToggleGO.GetComponent<Toggle>();
+        showEducationTugle.isOn = ShowEducation;
+    }
+    private void Start()
+    {
+        var showEducationTugle = showEducationToggleGO.GetComponent<Toggle>();
+        ShowEducation = showEducationTugle.isOn;
+    }
+    public void SetShowEducation()
+    {
+        ShowEducation = !ShowEducation;
+    }
+    private void Update()
+    {
+        test = ShowEducation;
+    }
+}
---
+++ Assets/Scripts/Message/LearnMessagesController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnMessagesController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(MainMenuManager.ShowEducation); 
+    }
+
+   
+}
