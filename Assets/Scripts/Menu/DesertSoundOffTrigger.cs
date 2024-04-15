cc0832d414a047d7e57f1ce3163b41b8a6dc706e
Almaz Zakariev <almazori228@gmail.com>
2024-04-15 21:35:05 +0300

    checkpoint button added

--- Assets/Scripts/Menu/DesertSoundOffTrigger.cs
+++ Assets/Scripts/Menu/DesertSoundOffTrigger.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class DesertSoundOffTrigger : MonoBehaviour
 {
-    //public AudioSource backGroundSound;
+    public AudioSource backGroundSound;
     public AudioSource desertSound;
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             desertSound.Pause();
-            //backGroundSound.Play();
+            backGroundSound.Play();
         }
     }
 }
