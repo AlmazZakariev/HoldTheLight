5520bbbc8c0c0ab56cc3200ea589a0a7581a425b
Almaz Zakariev <almazori228@gmail.com>
2024-04-15 11:18:54 +0300

    Scenes fixed, ui message small added

--- Assets/Scripts/Message/MessageManager.cs
+++ Assets/Scripts/Message/MessageManager.cs
@@ -7,12 +7,13 @@ using TMPro;
 
 public class MessageManager : MonoBehaviour
 {
-    public static Action<string> displayMessageEvent;
+    public static Action<string, bool> displayMessageEvent;
     public static Action disableMessageEvent;
     public TMP_Text messageText;
+    public TMP_Text messageSmallText;
 
     public Animator animator;
-    
+    public Animator animatorSmall;
 
     // Start is called before the first frame update
     void Start()
@@ -29,15 +30,26 @@ public class MessageManager : MonoBehaviour
         displayMessageEvent -= DisplayMessage;
         disableMessageEvent -= DisableMessage;
     }
-    private void DisplayMessage(string message)
-    {       
-        messageText.text = message; 
-        animator.SetInteger("State", 1);
-        Time.timeScale = 0;
+    private void DisplayMessage(string message, bool small)
+    {      
+        if (!small)
+        {
+            messageText.text = message;
+            animator.SetInteger("State", 1);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            messageSmallText.text = message;
+            animatorSmall.SetInteger("State", 1);
+            Time.timeScale = 0;
+        }
+        
     }
     private void DisableMessage()
     {
         animator.SetInteger("State", 0);
+        animatorSmall.SetInteger("State", 0);
         Time.timeScale = 1;
     }
 }
